1947274b4ac5aa5a41452e59dc446e0cc4e2352c
symbioids <parasshresthanever@gmail.com>
2024-05-30 19:02:33 +0545

    pani paryo aja

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
 
 
@@ -6,6 +7,7 @@ export default function Home() {
  <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
    
    <HeroSection />
+   <FeaturedCourses />
  </main>
   );
 }
---
+++ src/components/FeaturedCourses.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import courseData from "../data/music_courses.json"
+import Link from 'next/link'
+function FeaturedCourses() {
+  return (
+    <div className='py-12 bg-gray-900'>
+        <div className='text-center'>
+                <h2 className=''>FEATURED COURSES</h2>
+                <p className='mt-2 text-3xl leading-8 font-extrabold tracking-tight text-white sm:text-4xl'>Learn with  the Best</p>
+        </div>
+        <div>2</div>
+
+        <div className='mt-20 text-center'>
+                <Link  href={"/courses"} className='px-4 py-2 rounded border border-neutral-600 text-neutral-700 bg-white hover:bg-gray-100 transition duration-200' >
+                    View All Courses
+                </Link>
+        </div>
+        
+    </div>
+  )
+}
+
+export default FeaturedCourses
