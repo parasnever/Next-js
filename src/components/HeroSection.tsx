269f8dfd81366e94cee5bbb8f311aa29fd5c6c41
symbioids <parasshresthanever@gmail.com>
2024-05-30 11:13:44 +0545

    some changes

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -5,13 +5,13 @@ import { Button } from "./ui/moving-border";
 
 function HeroSection() {
   return (
+    
     <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
-
-<Spotlight
-
+          <Spotlight
         className="-top-40 left-0 md:left-60 md:-top-20"
         fill="white"
       />
+
 
       <div className="p-4 relative z-10 w-full text-center">
         <h1
@@ -22,7 +22,8 @@ function HeroSection() {
         >Dive into our comprehensive music courses and transform your musical journey today. Whether you're a beginner or looking to refine your skills, join us to unlock your true potential.</p>
         <div className="mt-4">
             <Link href={"/courses"}>
-               <Button>
+               <Button borderRadius="1.75rem"
+               className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800">
                Explore courses
                </Button>
             </Link>
