7b848c21e17cc7d2bd22e5bbed0099f7dcc0ab6e
symbioids <parasshresthanever@gmail.com>
2024-06-03 19:42:06 +0545

    fetured of the box

--- src/components/FeaturedCourses.tsx
+++ src/components/FeaturedCourses.tsx
@@ -1,11 +1,23 @@
+'use client'
 import React from 'react'
 import courseData from "../data/music_courses.json"
 import Link from 'next/link'
+import { BackgroundGradient } from './ui/background-gradient'
 
 
-interface Courses {}
+interface Courses {
+  
+  id: number,
+  title: string,
+  slug: string,
+  description: string,
+  price: number,
+  instructor:string,
+  isFeatured: boolean,
+ 
+}
 function FeaturedCourses() {
-  courseData.courses.filter(course => course.isFeatured)
+  const feturedCourses = courseData.courses.filter((course:Courses) => course.isFeatured)
 
   return (
     <div className='py-12 bg-gray-900'>
@@ -15,7 +27,18 @@ function FeaturedCourses() {
         </div>
         <div className='mt-10' >
           <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 justify-center'>
-            test
+            {feturedCourses.map((course:Courses)=>(
+              <div key={course.id} className="flex justify-center">
+               <BackgroundGradient className='flex flex-col  rounded-[22px] bg-white dark:bg-zinc-900 overflow-hidden h-full max-sm:'>
+                  <div className='p-4 sm:p-6 flex flex-col items-center text-center flex-grow'>
+                    <p className='text-lg sm:text-xl text-black mt-4 mb-4 dark:text-neutral-200'>{course.title}</p>
+                    <p className='text-sm text-neutral-600 dark:text-neutral-400 flex-grow'>{course.description}</p>
+                    <Link href={`/courses/${course.slug}`}> 
+                    Learn More</Link>
+                  </div>
+               </BackgroundGradient>
+              </div>
+            ))}
           </div>
         </div>
 
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -19,7 +19,7 @@ function HeroSection() {
         >Master the art of Music</h1>
         <p
         className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto"
-        >Dive into our comprehensive music courses and transform your musical journey today. Whether you're a beginner or looking to refine your skills, join us to unlock your true potential.</p>
+        >Dive into our comprehensive music courses and transform your musical journey today. Whether you&apos;re a beginner or looking to refine your skills, join us to unlock your true potential.</p>
         <div className="mt-4">
             <Link href={"/courses"}>
                <Button borderRadius="1.75rem"
