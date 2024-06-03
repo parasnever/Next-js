45a0d3213310247e7efd0fefda53f3ef36f0ddd8
symbioids <parasshresthanever@gmail.com>
2024-06-03 21:04:13 +0545

    parallel color and text effect;

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
    
    <HeroSection />
    <FeaturedCourses />
+   <WhyChooseUs />
  </main>
   );
 }
