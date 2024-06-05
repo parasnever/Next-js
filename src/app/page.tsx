a7380f96076b7a38ccecbd46d37b8134fba4e179
symbioids <parasshresthanever@gmail.com>
2024-06-05 12:38:26 +0545

    some

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
 import MusicSchoolTestimonialCards from "@/components/TestimonialCards";
+import UpcomingWebinar from "@/components/UpcomingWebinar";
 import WhyChooseUs from "@/components/WhyChooseUs";
 
 
@@ -12,6 +13,7 @@ export default function Home() {
    <FeaturedCourses />
    <WhyChooseUs />
    <MusicSchoolTestimonialCards />
+   <UpcomingWebinar />
  </main>
   );
 }
