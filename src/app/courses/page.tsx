ace92053bc9dbb4ac3191ab793f42810fe047869
kalix <parasshresthanever@gmail.com>
2024-06-07 11:02:50 +0545

    lasto

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -12,9 +12,9 @@ function page() {
   return (
     <div className="min-h-screen bg-black py-12  pt-36">
         <h1 className="text-lg md:text-7xl text-center font-sans font-bold mb-8 text-white">All Courses {CourseData.courses.length}</h1>
-        <div className="flex flex-wrap justify-center">
+        <div className="flex flex-wrap justify-center ">
             {CourseData.courses.map((course)=>(
-                <CardContainer className="inter-var">
+                <CardContainer className="inter-var  m-4">
                 <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
                   <CardItem
                     translateZ="50"
@@ -31,11 +31,11 @@ function page() {
                   </CardItem>
                   <CardItem translateZ="100" className="w-full mt-4">
                     <Image
-                      src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                      src={course.image}
                       height="1000"
                       width="1000"
                       className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
-                      alt="thumbnail"
+                      alt={course.image}
                     />
                   </CardItem>
                   <div className="flex justify-between items-center mt-20">
